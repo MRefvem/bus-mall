4c361f22e05d0d64649f7e6264af44a39754ffba
MichaelRefvem <mkrefvem@gmail.com>
2020-05-12 12:12:03 -0700

    added chart from today's demo

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ var currentRound = 0;
 
 var maxRounds = 25;
 
+var names = [];
+
+var votes = [];
 
 // Constructor Function
 function Product(src, alt, title){
@@ -100,6 +103,7 @@ function eventHandler(){
     console.log('max rounds', maxRounds);
     renderList();
     parent.removeEventListener('click', eventHandler);
+    makeNamesArray();
   }
 }
 
@@ -124,3 +128,58 @@ function renderList(){
   parent.appendChild(unorderedList);
 };
 
+
+
+// Function: set up empty names and votes arrays
+// Loop over all of my items and make an array of just the names of my items
+
+function makeNamesArray(){
+  for(var i = 0; i < allProducts.length; i++){
+    names.push(allProducts[i].title);
+    votes.push(allProducts[i].votes);
+  }
+  generateChart();
+}
+
+function generateChart(){
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: names,
+          datasets: [{
+              label: '# of Votes',
+              data: votes,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  });
+}
+
+
+// My Chart
